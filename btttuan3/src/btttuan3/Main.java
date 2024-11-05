19ad2dca69cd718e1aefda9672e9901c111670c3
mihow201 <phamtattu201@gmail.com>
2024-11-05 18:18:08 +0700

    tuan3

--- btttuan3/src/btttuan3/Main.java
+++ btttuan3/src/btttuan3/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Nhập số lượng sinh viên: ");
         int n = sc.nextInt();
         sc.nextLine();
